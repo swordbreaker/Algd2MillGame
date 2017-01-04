9fa6e768530bd0052da9baa900ca1e67ddf8711d
Janis <janis.angst@gmail.com>
2017-01-04 16:29:07 +0100

    Created grid for the board. Added cell on every placeable point and ViewModel for the board view.

---
+++ MillGame/ViewModels/MillBoardViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using MillGame.Commands;
+using MillGame.Views;
+
+namespace MillGame.ViewModels
+{
+    class MillBoardViewModel
+    {
+    }
+}
