547696e56b3efddb1417d483e5f24408e2bd1854
Janis <janis.angst@gmail.com>
2017-01-06 17:55:09 +0100

    Added empty ellipse which simulate a move by clicking on the field.
    TODO: adjust the stoneplaces on the board and add clickbehaviour for different states

--- MillGame/ViewModels/MillBoardViewModel.cs
+++ MillGame/ViewModels/MillBoardViewModel.cs
@@ -3,13 +3,73 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
 using MillGame.Commands;
 using MillGame.Views;
 
 namespace MillGame.ViewModels
 {
-    class MillBoardViewModel
+    public class MillBoardViewModel
     {
+        private Brush _previousFill = null;
+        private Ellipse _previousEllipse = null;
+
+        private readonly SolidColorBrush black = new SolidColorBrush(Colors.Black);
+        private readonly SolidColorBrush red = new SolidColorBrush(Colors.Red);
+        private readonly SolidColorBrush transparent = new SolidColorBrush(Colors.Transparent);
+
+        public MillBoardViewModel()
+        {
+            
+        }
+
+        public bool StoneClick(object sender, MouseEventArgs e, bool secondClick)
+        {
+            Ellipse ellipse = sender as Ellipse;
+
+            if (!secondClick)
+            {
+                if(ellipse.Fill.ToString() == transparent.ToString())
+                {
+                    return secondClick;
+                }
+
+                _previousEllipse = sender as Ellipse;
+                _previousFill = ellipse.Fill;
+
+                ellipse.Fill = new SolidColorBrush(Colors.Green);
+            }
+            else
+            {
+                int uid;
+                Int32.TryParse(ellipse.Parent.GetValue(UIElement.UidProperty).ToString(), out uid);
+
+                if (ellipse.Fill.ToString() == red.ToString() || ellipse.Fill.ToString() == black.ToString())
+                {
+                    return secondClick;
+                }
+                else if((uid <= 58 && uid >= 50) || (uid <= 68 && uid >= 60))
+                {
+                    return secondClick;
+                }
+                else if(ellipse.Name == _previousEllipse.Name)
+                {
+                    return secondClick;
+                }
+                else
+                {
+                    ellipse.Fill = _previousFill;
+                    ellipse.Name = _previousEllipse.Name;
+                    _previousFill = null;
+                    _previousEllipse.Fill = new SolidColorBrush(Colors.Transparent);
+                    _previousEllipse = null;
+                }
+            }
+
+            return secondClick = !secondClick;
+        }
     }
 }
--- MillGame/Views/MillBoard.xaml.cs
+++ MillGame/Views/MillBoard.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MillGame.ViewModels;
 
 namespace MillGame.Views
 {
@@ -20,9 +21,18 @@ namespace MillGame.Views
     /// </summary>
     public partial class MillBoard : UserControl
     {
+        public static MillBoardViewModel ViewModel { get; private set; }
+        private bool secondClick = false;
+
         public MillBoard()
         {
+            ViewModel = new MillBoardViewModel();
             InitializeComponent();
+        }
+
+        private void MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            secondClick = ViewModel.StoneClick(sender, e, secondClick);
         }
     }
 }
